709d9e63db000e255702cb77f69286c070600874
rodrigo <lenilsonrodrigo123@gmail.com>
2024-02-25 11:10:34 -0300

    content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,16 @@
+export const datafake = [
+  {
+    "id":"1",
+    "title": "Novo filme do Homem de ferro é anunciado",
+    "description": "chega em abril aos cinemas",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=webp"
+
+  },
+  {
+    "id":"2",
+    "title": "Novas séries",
+    "description": "Disney + surpreende fãs com a chegada de 4 novas séries em seu catalago",
+    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2021/04/Upcoming-Marvel-TV-Shows-910x474.jpg"
+
+  }
+]
